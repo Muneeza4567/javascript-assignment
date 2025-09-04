072c878a40a1f31d878cfa1c3a029b66f65d11bc
Muneeza4567 <Muneezamughal211@gmail.com>
2025-09-04 21:50:03 +0500

    completed

---
+++ Assignment no 3/app.js
@@ -0,0 +1,33 @@
+// Qno:1 Declare a variable called age & assign to it your age. Show
+// your age in an alert box. 
+
+let studentAge = "15"
+alert("I am " + studentAge + " year's old")
+
+// Qno: 2 Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page.Show his / her
+// number of visits on your web page.For example: “You
+// have visited this site N times”.
+
+let visited = "14"
+alert("You have visited this site " + visited + " times")
+
+// Qno:3 Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+
+let birthYear = "2009"
+document.write  ("My birth year is  " + birthYear )
+document.write ("<br> <br>")
+
+
+// Qno: 4. A visitor visits an online clothing store www.xyzClothing.com . Write a script to store in variables the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to order
+// Show the following message in your browser: “John Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+let visitorName = "John Doe"
+let ProductTitle = " T-shirt"
+let quantity = " 5 "
+
+document.write (visitorName + " orderd " + quantity + ProductTitle + "(s) on XYZ Clothing store.")
---
+++ Assignment no 4/app.js
@@ -0,0 +1,39 @@
+// ------------- Chapter # 04  VARIABLE NAMES: LEGAL & ILLEGA -------------
+
+// Qno:1  Declare 3 variables in one statement.
+
+// let  name,number,email;
+
+// Qno:2 2. Declare 5 legal & 5 illegal variable names.
+
+// legal variable names
+// 1. let name;
+// 2. let userAge;
+// 3. let muneeza_Akbar;
+// 4. let $prise;
+// 5. let Akbar123;
+
+
+// illegal variable names
+// 1. let 12Muneeza;                    ❌ Cannot start with a digit
+// 2. let Hanish fatima;                ❌ Spaces are not allowed
+// 3. let var;                          ❌ cannot use js keyword 
+// 4. let user-name;                    ❌ Hyphens are not allowed
+// 5. let @muneeza;                     ❌ Cannot start with special characters like @ only use this _,$
+
+
+// Qno:3 Display this in your browser
+// a) A heading stating “Rules for naming JS variables”
+// b) Variable names can only contain ______, ______,______ and ______. For example $my_1stVariable
+// c) Variables must begin with a ______, ______ or _____. For example $name, _name or name
+// d) Variable names are case _________
+// e) Variable names should not be JS _________
+
+//  document.write ("<h1>" +  "Rules for naming JS variables"  + "</h1>")
+//  document.write (" Variable names can only contain numbers , latter's , $ and _  For example $my_1stVariable </br> ")
+//  document.write (" Variables must begin with a $ , _ , latters  For example $name, _name or name </br> ")
+//  document.write (" Variable names are case sensitive </br>")
+//  document.write ("  Variable names should not be JS keywords")
+
+
+// ------------- Chapter # 04 End -------------
